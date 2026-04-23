29b714425e834a6589c990d6bf762ed78751a8e9
MehtabJaveed <MJaveed1@uclan.ac.uk>
2026-04-23 15:54:01 +0100

    update 3 password encrypt
    so it keeps giving a fernet invalidToken, after looking at it its probably due to the password_manager.txt being empty or having bad data.
    
    - Need to update view() to handle these possibilities.
    
    23/04/2026

--- public/password_manager.py
+++ public/password_manager.py
@@ -1,4 +1,25 @@
-master_password = input("Enter your password: ")
+from cryptography.fernet import Fernet
+
+"""
+# This is a function to generate a key and save it into a file
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+"""
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+# master_password = input("Enter your password: ")
+# we are adding the master password to the key to make it more secure
+key = load_key()  # + master_password.encode()
+fer = Fernet(key)
 
 
 def view():
@@ -7,8 +28,10 @@ def view():
         for line in f.readlines():
             data = line.rstrip()  # rstrip removes the new line character
             # split the data into user and password
-            user, password = data.split("|")
-            print("Account: " + user + " | Password: " + password)
+            user, passw = data.split("|")
+            # we need to encode the password before we can decrypt it, and then decode it after we decrypt it to get the original password
+            print("Account: ", user, " | Password: ",
+                  fer.decrypt(passw.encode()).decode())
 
 
 def add():
@@ -17,7 +40,7 @@ def add():
 
     # a means append, w means write r means read, with means it will automatically close the file after we are done
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + password + "\n")  # \n means new line
+        f.write(name + "|" + fer.encrypt(password.encode()).decode() + "\n")
 
 
 while True:
